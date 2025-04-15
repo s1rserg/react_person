c34097d0ec39ad372c1872a5290ce85722a05ed1
sirserg <sirserg05@gmail.com>
2025-04-15 20:44:59 +0300

    feat: add task solution

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,20 @@
-// export const Person = ({ person }) => ();
+import './Person.scss';
+
+export const Person = ({ person }) => {
+  let partnerText;
+
+  if (person.isMarried) {
+    partnerText =
+      person.sex === 'm'
+        ? `${person.partnerName} is my wife`
+        : `${person.partnerName} is my husband`;
+  }
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">{`My name is ${person.name}`}</h2>
+      {person.age && <p className="Person__age">{`I am ${person.age}`}</p>}
+      <p className="Person__partner">{partnerText}</p>
+    </section>
+  );
+};
