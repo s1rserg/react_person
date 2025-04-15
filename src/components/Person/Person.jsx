16a3cbf613662fb4cbeda4ecfe42cc9f659eab9c
sirserg <sirserg05@gmail.com>
2025-04-15 23:06:09 +0300

    fix: add person props destructuring

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,21 +1,23 @@
 import './Person.scss';
 
 export const Person = ({ person }) => {
+  const { name, age, sex, isMarried, partnerName } = person;
+
   let partnerText;
 
-  if (person.isMarried) {
+  if (isMarried) {
     partnerText =
-      person.sex === 'm'
-        ? `${person.partnerName} is my wife`
-        : `${person.partnerName} is my husband`;
+      sex === 'm'
+        ? `${partnerName} is my wife`
+        : `${partnerName} is my husband`;
   } else {
     partnerText = 'I am not married';
   }
 
   return (
     <section className="Person">
-      <h2 className="Person__name">{`My name is ${person.name}`}</h2>
-      {person.age && <p className="Person__age">{`I am ${person.age}`}</p>}
+      <h2 className="Person__name">{`My name is ${name}`}</h2>
+      {age && <p className="Person__age">{`I am ${age}`}</p>}
       <p className="Person__partner">{partnerText}</p>
     </section>
   );
